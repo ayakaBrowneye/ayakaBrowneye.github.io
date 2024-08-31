e9796b6dce0ade6d0b6ad8a2fc9c8a837ad9bd7a
ayakaBrowneye <ayaka.browneye@gmail.com>
2024-08-31 20:31:56 +0900

    TOPページ

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
+// 下からふわっと
+window.addEventListener("scroll", function () {
+  const elements = document.querySelectorAll(".fadeUpTrigger");
+  console.log(elements);
+  elements.forEach(function (element) {
+    const elemPos = element.getBoundingClientRect().top + window.scrollY - 100;
 
+    const scroll = window.scrollY;
+    const windowHeight = window.innerHeight;
+
+    if (scroll >= elemPos - windowHeight) {
+      element.classList.add("fadeUp");
+    } else {
+      element.classList.remove("fadeUp");
+    }
+  });
+});
